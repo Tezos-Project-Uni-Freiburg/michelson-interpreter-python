8b238f2727998f138f873dc512a0b05600f63934
Hasan Berkay Çağır <berkay@cagir.com.tr>
2022-07-17 14:48:06 +0200

    Fill in classes and add type annots

--- _types.py
+++ _types.py
@@ -1,18 +1,41 @@
+from dataclasses import dataclass
+
+
 class CustomException(Exception):
     ...
 
 
+@dataclass
 class Data:
-    ...
+    prim: str = ""
+    value: list = []
+    name: str = ""
+    raw: dict = {}
+
+    def __post_init__(self):
+        if self.raw != {}:
+            ...
 
 
+@dataclass
 class Delta:
-    ...
+    removed: list = []
+    added: list = []
 
 
+@dataclass
 class State:
-    ...
+    account: str = ""
+    address: str = ""
+    amount: int = 0
+    entrypoint: str = "default"
+    gas_limit: int = 0
+    id: int = 0
+    timestamp: int = 0
 
 
+@dataclass
 class Step:
-    ...
+    delta: Delta
+    instruction: list = []
+    stack: list = []
--- michelson-interpreter.py
+++ michelson-interpreter.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+import io
 import json
+import pprint
 import re
 import subprocess
 import sys
@@ -17,19 +19,25 @@ def excepthook(type, value, traceback):
 sys.excepthook = excepthook
 
 
-def michelson_interpreter(script, parameter, storage, state):
-    s_raw = subprocess.run(
+def michelson_interpreter(
+    script: io.TextIOWrapper, parameter: str, storage: str, state: State
+):
+    global current_state
+    current_state = state
+    s = subprocess.run(
         ["./ext/michelson-parser-wrapper/bin/michelson-parser.js"],
         capture_output=True,
         encoding="utf-8",
         stdin=script,
     )
-    s = json.loads(re.sub(r"\\\\\"", '\\"', s_raw.stdout).strip()[1:-1])
+    s = json.loads(re.sub(r"\\\\\"", '\\"', s.stdout).strip()[1:-1])
     if len(s) > 1:
         raise Exception("Multiple parsings!")
     s = json.loads(s[0])
+    p_raw, s_raw, c_raw = s[0], s[1], s[2]
+    pprint.pp(c_raw)
 
 
 if __name__ == "__main__":
-    with open(sys.argv[1]) as f:
-        michelson_interpreter(f, ..., ..., ...)
+    with open(sys.argv[1], encoding="utf-8") as f:
+        michelson_interpreter(f, "...", "...", State())
